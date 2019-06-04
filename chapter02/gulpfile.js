6b5cd04b4b6c75450cd777c80411da85b233bbe6
unknown <gelos2012@gmail.com>
2019-06-05 07:52:42 +0900

    ~gulp-babel setting

--- chapter02/gulpfile.js
+++ chapter02/gulpfile.js
@@ -1,7 +1,16 @@
 //걸프 의존성 사용
 const gulp = require('gulp');
-
+const babel = require('gulp-babel');
 // 걸프 작업 지정
 gulp.task('default', function(){
 
+    //노드 소스
+    gulp.src("es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
+
+    //브라우저 소스
+    gulp.src("public/es6/**/*.js")
+        .pipe(babel())
+        .pipe("public/dist");
 });
