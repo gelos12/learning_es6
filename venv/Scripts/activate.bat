13ae6004eaf575869970434c042d039a0dd26f73
unknown <gelos2012@gmail.com>
2019-06-05 00:21:23 +0900

    virtualenv nodeenv setting

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,35 +1,25 @@
 @echo off
-
-set "VIRTUAL_ENV=C:\0study\learning_es6\venv"
-
+set "NODE_VIRTUAL_ENV=c:\0study\learning_es6\venv"
+if not defined PROMPT (
+    set "PROMPT=$P$G"
+)
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
-) else (
-    if not defined PROMPT (
-        set "PROMPT=$P$G"
-    )
-    if not defined VIRTUAL_ENV_DISABLE_PROMPT (
-        set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
-    )
 )
-if not defined VIRTUAL_ENV_DISABLE_PROMPT (
-    set "PROMPT=(venv) %PROMPT%"
+if defined _OLD_VIRTUAL_NODE_PATH (
+    set "NODE_PATH=%_OLD_VIRTUAL_NODE_PATH%"
 )
-
-REM Don't use () to avoid problems with them in %PATH%
-if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
-    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
-:ENDIFVHOME
-
-set PYTHONHOME=
-
-REM if defined _OLD_VIRTUAL_PATH (
-if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(venv) %PROMPT%"
+if defined NODE_PATH (
+    set "_OLD_VIRTUAL_NODE_PATH=%NODE_PATH%"
+    set NODE_PATH=
+)
+if defined _OLD_VIRTUAL_PATH (
     set "PATH=%_OLD_VIRTUAL_PATH%"
-:ENDIFVPATH1
-REM ) else (
-if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
+) else (
     set "_OLD_VIRTUAL_PATH=%PATH%"
-:ENDIFVPATH2
+)
+set "PATH=%NODE_VIRTUAL_ENV%\Scripts;%PATH%"
+:END
 
-set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
