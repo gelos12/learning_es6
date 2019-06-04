e44cf55e9e6e9e1711a073ae0d7e73fa4c4127f4
unknown <gelos2012@gmail.com>
2019-06-04 20:22:49 +0900

    chapter hello World

--- cahpter01/chapter01.js
+++ cahpter01/chapter01.js
@@ -20,6 +20,13 @@ $(document).ready(function(){
     //     c.fillColor = 'red';
     // };
 
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200,200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = "hello world";
     paper.view.draw();
     console.log("chapter01.js loaded");
-})
+});
