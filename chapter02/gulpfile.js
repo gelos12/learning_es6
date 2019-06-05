60797ae8e3f7e6aa516f05df2d1d3246a4d66aca
unknown <gelos2012@gmail.com>
2019-06-05 21:45:17 +0900

    2장 완료

--- chapter02/gulpfile.js
+++ chapter02/gulpfile.js
@@ -1,8 +1,14 @@
 //걸프 의존성 사용
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
 // 걸프 작업 지정
-gulp.task('default', function(){
+gulp.task('default', function(done){
+
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
 
     //노드 소스
     gulp.src("es6/**/*.js")
@@ -12,5 +18,7 @@ gulp.task('default', function(){
     //브라우저 소스
     gulp.src("public/es6/**/*.js")
         .pipe(babel())
-        .pipe("public/dist");
+        .pipe(gulp.dest("public/dist"));
+
+    done();
 });
